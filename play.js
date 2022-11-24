7dce0d680ab62a837da694d20b03c2737fcda882
daejung90 <djung.ca@gmail.com>
2022-11-23 23:52:17 -0500

    change to import function connect

--- play.js
+++ play.js
@@ -1,17 +1,2 @@
-const net = require ('net');
+const connection = require('./client.js');
 
-const connect = function () {
-    const conn = net.createConnection ({
-        host: '165.227.47.243',
-        port: 50541,
-    });
-    conn.setEncoding('utf8');
-
-    conn.on('data', (data) => {
-        console.log(data)
-    })
-
-    return conn;
-};
-console.log('Connecting...');
-connect()
