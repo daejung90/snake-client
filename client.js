c27d7dbcc893f1d84d42e96216a1ab215b132f16
daejung90 <djung.ca@gmail.com>
2022-11-24 20:50:06 -0500

    first commit

--- client.js
+++ client.js
@@ -1,25 +1,55 @@
 const net = require ('net');
+const { stdin } = require('process');
 
 const connect = function () {
     const conn = net.createConnection ({
-        host: '165.227.47.243',
-        port: 50541,
+        host: '192.168.1.192',
+        port: '50541',
     });
     conn.on('connect', () => {
-        console.log("Succesfully connected to game server!")
+        console.log("Succesfully connected to game server!");
+        conn.write("Name: Dan")
+        conn.setInterval(() => {
+            conn.write("Move: up");
+        }, 2000);
+        conn.setInterval(() => {
+            conn.write('Move: down');
+        }, 3000);
+        conn.setInterval(() => {
+            conn.write('Move: left');
+        }, 4000);
+        conn.setInterval(() => {
+            conn.write('Move: right');
+        }, 5000)
+        // conn.write('Move: up');
+        // conn.write('Move: down');
+        // conn.write('Move: left')
+        // conn.write('Move: right');
+        // }
+
     });
 
     conn.setEncoding('utf8');
 
     conn.on('data', (data) => {
         console.log(data)
-    })
-    conn.on('adata', (data) => {
-        conn.write("Name: Dan")
-    })
+    });
+    
+    
 
     return conn;
 };
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
+
+
+
 console.log('Connecting...');
 connect()
 
